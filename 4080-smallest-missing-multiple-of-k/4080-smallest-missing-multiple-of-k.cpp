e178bdb88102dbad33292d8a374d28a38d93106a
Subham_Sharrma <subhamsharrma@gmail.com>
2025-10-21 12:59:32 +0530

    Time: 3 ms (45.46%), Space: 24 MB (87%) - LeetHub

--- 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int x = k;  
-        for (auto i : nums) {
-            if (i == x) x += k;
+        sort(nums.begin() , nums.end()) ; 
+
+        int x = k ;
+        for(auto i : nums) {
+            if(i == x ) x = x + k ; 
         }
-        return x;
+
+
+
+        return x ;
+
     }
 };
