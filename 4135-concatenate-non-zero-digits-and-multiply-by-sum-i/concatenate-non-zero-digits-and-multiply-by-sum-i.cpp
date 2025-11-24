121448eff6465289459a2d8f21706e1581483c89
Subham_Sharrma <subhamsharrma@gmail.com>
2025-11-24 13:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (100.00%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-
-        vector<int> vec;
-        string s;
-
-        while (n > 0)
-        {
-            int digit = n % 10;
-            if (digit != 0)
-            {
-                vec.push_back(digit);
+        string s = to_string(n);
+        long sum = 0, v = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            int c = s[i] - '0';
+            if(c > 0) {
+                sum += c;
+                v =  v * 10 + c;
             }
-            n /= 10;
         }
-
-        if (vec.empty()) return 0;  // IMPORTANT FIX
-
-        reverse(vec.begin(), vec.end());
-        int sums = accumulate(vec.begin(), vec.end(), 0);
-
-
-        for (int num : vec)
-        {
-            s += to_string(num);
-        }
-
-        long long concatenated = stoll(s);
-        return sums * concatenated ;
+        return sum * v;
         
     }
 };
