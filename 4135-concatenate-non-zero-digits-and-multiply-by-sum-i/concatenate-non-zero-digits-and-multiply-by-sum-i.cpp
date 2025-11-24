ae75003b964d7185bc96e7500993044685525070
Subham_Sharrma <subhamsharrma@gmail.com>
2025-11-24 13:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (100.00%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,3 +1,38 @@
+
+// class Solution {
+// public:
+//     long long sumAndMultiply(int n) {
+
+//         vector<int> vec;
+//         string s;
+
+//         while (n > 0)
+//         {
+//             int digit = n % 10;
+//             if (digit != 0)
+//             {
+//                 vec.push_back(digit);
+//             }
+//             n /= 10;
+//         }
+
+//         if (vec.empty()) return 0;  // IMPORTANT FIX
+
+//         reverse(vec.begin(), vec.end());
+//         int sums = accumulate(vec.begin(), vec.end(), 0);
+
+
+//         for (int num : vec)
+//         {
+//             s += to_string(num);
+//         }
+
+//         long long concatenated = stoll(s);
+//         return sums * concatenated ;
+        
+//     }
+// };
+
 class Solution {
 public:
     long long sumAndMultiply(int n) {
