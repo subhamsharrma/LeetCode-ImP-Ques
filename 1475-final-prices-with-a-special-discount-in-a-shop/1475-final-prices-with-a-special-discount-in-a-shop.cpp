c64f75ab1504221d55841d4ab1d1c797459e89d9
Subham Sharrma <subhamsharrma@gmail.com>
2026-01-03 07:45:01 +0530

    Time: 0 ms (100%), Space: 14.2 MB (55.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,16 +1,39 @@
+// class Solution {
+// public:
+//     vector<int> finalPrices(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<int> ans = prices;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (prices[j] <= prices[i]) {
+//                     ans[i] = prices[i] - prices[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
         vector<int> ans = prices;
+        stack<int> st; // stores indices
 
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (prices[j] <= prices[i]) {
-                    ans[i] = prices[i] - prices[j];
-                    break;
-                }
+            while (!st.empty() && prices[st.top()] >= prices[i]) {
+                ans[st.top()] -= prices[i];
+                st.pop();
             }
+            st.push(i);
         }
         return ans;
     }
