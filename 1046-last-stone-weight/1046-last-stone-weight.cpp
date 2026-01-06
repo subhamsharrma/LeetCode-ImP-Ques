348990cc3061fdead557a85808a9e572f1eae146
Subham Sharrma <subhamsharrma@gmail.com>
2026-01-06 12:54:14 +0530

    Time: 0 ms (100%), Space: 9.8 MB (96.36%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+
+        while (pq.size() > 1) {
+            int y = pq.top(); pq.pop();  // heaviest
+            int x = pq.top(); pq.pop();  // second heaviest
+
+            if (y != x) {
+                pq.push(y - x);
+            }
+        }
+
+        if (pq.empty()) return 0;
+        return pq.top();
+    }
+};
