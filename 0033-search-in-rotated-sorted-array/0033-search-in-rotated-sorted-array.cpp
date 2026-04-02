5cd7736ee0eee557e5c170fdd3107aef36391956
Subham Sharrma <subhamsharrma@gmail.com>
2026-04-02 22:58:55 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,14 +4,11 @@ public:
         int n = nums.size() ; 
 
         for (int i = 0; i < n; i++) {
-            // Check if the current element is the target
-            if (nums[i] == target)
-                // Return the index if the target is found
-                return i;
+             if (nums[i] == target)
+                 return i;
         }
 
-        // Return -1 if the target is not found
-        return -1;
+         return -1;
         
     }
 };
