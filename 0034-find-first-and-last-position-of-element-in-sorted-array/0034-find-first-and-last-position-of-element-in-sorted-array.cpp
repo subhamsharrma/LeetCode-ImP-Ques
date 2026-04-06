401c8e68ddefcee1eb3da67faa3aefc1f9c3ba51
Subham Sharrma <subhamsharrma@gmail.com>
2026-04-06 23:06:27 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.77%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int st = 0, ed = nums.size() - 1;
-        int mid = 0;
+        int buvh = 0;
         vector<int> res(2, -1);
 
         // Find first occurrence
         while (st <= ed) {
-            mid = ed - (ed - st) / 2;
+            buvh = ed - (ed - st) / 2;
 
-            if (nums[mid] == target) {
-                if (mid == 0 || nums[mid - 1] != target) {
-                    res[0] = mid;
+            if (nums[buvh] == target) {
+                if (buvh == 0 || nums[buvh - 1] != target) {
+                    res[0] = buvh;
                     break;
                 } else {
-                    ed = mid - 1;
+                    ed = buvh - 1;
                 }
-            } else if (nums[mid] < target) {
-                st = mid + 1;
+            } else if (nums[buvh] < target) {
+                st = buvh + 1;
             } else {
-                ed = mid - 1;
+                ed = buvh - 1;
             }
         }
 
@@ -28,19 +28,19 @@ public:
 
         // Find last occurrence
         while (st <= ed) {
-            mid = ed - (ed - st) / 2;
+            buvh = ed - (ed - st) / 2;
 
-            if (nums[mid] == target) {
-                if (mid == nums.size() - 1 || nums[mid + 1] != target) {
-                    res[1] = mid;
+            if (nums[buvh] == target) {
+                if (buvh == nums.size() - 1 || nums[buvh + 1] != target) {
+                    res[1] = buvh;
                     break;
                 } else {
-                    st = mid + 1;
+                    st = buvh + 1;
                 }
-            } else if (nums[mid] < target) {
-                st = mid + 1;
+            } else if (nums[buvh] < target) {
+                st = buvh + 1;
             } else {
-                ed = mid - 1;
+                ed = buvh - 1;
             }
         }
 
