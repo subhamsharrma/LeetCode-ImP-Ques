a170588a1cd0bc1cbd910c4e97edb9660e8b2f93
Subham Sharrma <subhamsharrma@gmail.com>
2026-04-09 23:27:59 +0530

    Time: 5 ms (33.94%), Space: 25.7 MB (15.61%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,7 +3,7 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_set<char> rows[9];
         unordered_set<char> cols[9];
-        unordered_set<char> boxes[9];
+        unordered_set<char> ad[9];
 
         for (int r = 0; r < 9; ++r) {
             for (int c = 0; c < 9; ++c) {
@@ -14,13 +14,13 @@ public:
                 char value = board[r][c];
                 int boxIndex = (r / 3) * 3 + (c / 3);
 
-                if (rows[r].count(value) || cols[c].count(value) || boxes[boxIndex].count(value)) {
+                if (rows[r].count(value) || cols[c].count(value) || ad[boxIndex].count(value)) {
                     return false;
                 }
 
                 rows[r].insert(value);
                 cols[c].insert(value);
-                boxes[boxIndex].insert(value);
+                ad[boxIndex].insert(value);
             }
         }
 
