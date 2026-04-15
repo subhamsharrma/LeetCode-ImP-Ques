1a601fac0b7ae94508acc76e097de7fe3d605dc8
Subham Sharrma <subhamsharrma@gmail.com>
2026-04-15 23:45:48 +0530

    Time: 3 ms (85.06%), Space: 9.9 MB (61.74%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -10,19 +10,19 @@ public:
 
 private:
     string describe(const string& s) {
-        string res = "";
+        string ddddddddddddd = "";
         int count = 1;
 
         for (int i = 1; i < s.length(); ++i) {
             if (s[i] == s[i - 1]) {
                 count++;
             } else {
-                res += to_string(count) + s[i - 1];
+                ddddddddddddd += to_string(count) + s[i - 1];
                 count = 1;
             }
         }
 
-        res += to_string(count) + s.back();
-        return res;
+        ddddddddddddd += to_string(count) + s.back();
+        return ddddddddddddd;
     }
 };
