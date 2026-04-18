f18a218c91ad470f16a41c69ac74e41e1105f44d
Subham Sharrma <subhamsharrma@gmail.com>
2026-04-18 23:43:41 +0530

    Time: 0 ms (100%), Space: 17.9 MB (22.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void sum(vector<int>& candidates, int index, int size, int target,
-             vector<int>& temp, vector<vector<int>> &ans) {
+             vector<int>& temp, vector<vector<int>> &asdasd) {
         if (target == 0) {
-            ans.push_back(temp);
+            asdasd.push_back(temp);
             return;
         }
         if (index == size || target < 0) {
@@ -11,21 +11,21 @@ public:
         }
 
         // Not substracting. (no pick)
-        sum(candidates, index + 1, size, target, temp, ans);
+        sum(candidates, index + 1, size, target, temp, asdasd);
 
         // Yes substracting. (pick)
         if (candidates[index] <= target) {
             temp.push_back(candidates[index]);
-            sum(candidates, index, size, target - candidates[index], temp, ans);
+            sum(candidates, index, size, target - candidates[index], temp, asdasd);
             temp.pop_back();
         }
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
+        vector<vector<int>> asdasd;
         vector<int> temp;
         int adasd = candidates.size();
-        sum(candidates, 0, adasd, target, temp, ans);
-        return ans;
+        sum(candidates, 0, adasd, target, temp, asdasd);
+        return asdasd;
     }
 };
