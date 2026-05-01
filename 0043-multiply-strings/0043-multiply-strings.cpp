1122b305e723cd0448e88e6faddca98486dd3755
Subham Sharrma <subhamsharrma@gmail.com>
2026-05-01 23:21:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (90.21%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -5,24 +5,24 @@ public:
             return "0";
         }
 
-        vector<int> aaaa(num1.size() + num2.size(),0);
+        vector<int> bbbb(num1.size() + num2.size(),0);
         for(int i=num1.size()-1; i>=0; i--){
             for(int j=num2.size()-1; j>=0; j--){
-                aaaa[i+j+1] += (num1[i] - '0') * (num2[j] - '0');
-                aaaa[i+j] += aaaa[i+j+1]/10;
-                aaaa[i+j+1] %= 10;  
+                bbbb[i+j+1] += (num1[i] - '0') * (num2[j] - '0');
+                bbbb[i+j] += bbbb[i+j+1]/10;
+                bbbb[i+j+1] %= 10;  
             }
         }
         
         // now remove the leading 0's
         int i=0;
-        while(aaaa[i] == 0){
+        while(bbbb[i] == 0){
             i++;
         }
 
         string ans = "";
-        while(i<aaaa.size()){
-            ans.push_back(aaaa[i] + '0');
+        while(i<bbbb.size()){
+            ans.push_back(bbbb[i] + '0');
             i++;
         }
         return ans;
