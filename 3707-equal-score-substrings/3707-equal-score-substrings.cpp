05cb903dd06de720541d3233aa8b57faf904cf08
Subham Sharrma <subhamsharrma@gmail.com>
2026-05-30 23:47:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (55.56%) - LeetHub

---
+++ 3707-equal-score-substrings/3707-equal-score-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool scoreBalance(string s) {
+        int n=s.size();
+        int total=0;
+        for(int i=0;i<n;i++){
+            total+=s[i]-'a'+1;
+        }
+        int left=0;
+        for(int i=0;i<n-1;i++){
+            left+=s[i]-'a'+1;
+           int right=total-left;
+            if(left==right){
+                return true;
+            }
+        }
+        return false;
+    }
+};
