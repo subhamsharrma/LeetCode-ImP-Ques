5434b24b7ef3121e95c02018abd8afd5893bd49d
Subham Sharrma <subhamsharrma@gmail.com>
2026-06-02 23:33:24 +0530

    Time: 146 ms (12.89%), Space: 292.6 MB (36.08%) - LeetHub

---
+++ 3709-design-exam-scores-tracker/3709-design-exam-scores-tracker.cpp
@@ -0,0 +1,15 @@
+class ExamTracker {
+public:
+    vector<long long> pre{0}, times{0};
+
+    void record(int time, int score) {
+        pre.push_back(pre.back() + score);
+        times.push_back(time);
+    }
+
+    long long totalScore(int startTime, int endTime) {
+        int i = lower_bound(times.begin(), times.end(), startTime) - times.begin();
+        int j = upper_bound(times.begin(), times.end(), endTime) - times.begin();
+        return pre[j - 1] - pre[i - 1];
+    }
+};
