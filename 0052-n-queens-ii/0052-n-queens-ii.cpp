7259251e8793f5e62cf400284219cbbb1b22e786
Subham Sharrma <subhamsharrma@gmail.com>
2026-06-13 23:36:45 +0530

    Time: 0 ms (100%), Space: 7.7 MB (94.86%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -2,13 +2,13 @@ int N;
 static bool col[9] = {false}, diag[17] = {false}, adiag[17] = {false};
 class Solution {
 private:
-    inline int dfs(int i){
+    inline int bfd(int i){
         if(i == N) return 1;
         int cnt = 0;
         for(int j = 0; j < N; ++j){
             if(col[j] || diag[i + N - j - 1] || adiag[i + j]) continue;
             col[j] = diag[i + N - j - 1] = adiag[i + j] = true;
-            cnt += dfs(i + 1);
+            cnt += bfd(i + 1);
             col[j] = diag[i + N - j - 1] = adiag[i + j] = false;
         }
         return cnt;
@@ -18,6 +18,6 @@ public:
         if(n == 1) return 1;
         if(n < 4) return 0;
         N = n;
-        return dfs(0);
+        return bfd(0);
     }
 };
