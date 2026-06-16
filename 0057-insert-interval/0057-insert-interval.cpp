f6d56d5098ba662436ea8cca45809a12e2ed2307
Subham Sharrma <subhamsharrma@gmail.com>
2026-06-16 23:24:54 +0530

    Time: 3 ms (30%), Space: 21.7 MB (75.3%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,17 +4,17 @@ public:
         intervals.push_back(newInterval);
         sort(intervals.begin(), intervals.end());
 
-        vector<vector<int>> brs;
-        brs.push_back(intervals[0]);
+        vector<vector<int>> brsa;
+        brsa.push_back(intervals[0]);
 
         for (int i = 1; i < intervals.size(); ++i) {
-            if (brs.back()[1] >= intervals[i][0]) {
-                brs.back()[1] = max(brs.back()[1], intervals[i][1]);
+            if (brsa.back()[1] >= intervals[i][0]) {
+                brsa.back()[1] = max(brsa.back()[1], intervals[i][1]);
             } else {
-                brs.push_back(intervals[i]);
+                brsa.push_back(intervals[i]);
             }
         }
 
-        return brs;        
+        return brsa;        
     }
 };
