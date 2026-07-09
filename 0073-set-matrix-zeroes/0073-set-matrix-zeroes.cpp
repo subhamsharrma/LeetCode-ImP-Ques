23b7a46e822f904afcacb87e77d820983abc128f
Subham Sharrma <subhamsharrma@gmail.com>
2026-07-09 22:44:28 +0530

    Time: 28 ms (10.33%), Space: 21.5 MB (13.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,12 +4,12 @@ public:
         int m = matrix.size(), n = matrix[0].size();
         vector<vector<int>> copy = matrix; // O(mn) space
 
-        for(int i = 0; i < m; i++) {
+        for(int ij = 0; ij < m; ij++) {
             for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == 0) {
-                    // zero out row i
+                if(matrix[ij][j] == 0) {
+                    // zero out row ij
                     for(int col = 0; col < n; col++) 
-                        copy[i][col] = 0;
+                        copy[ij][col] = 0;
                     // zero out col j
                     for(int row = 0; row < m; row++) 
                         copy[row][j] = 0;
