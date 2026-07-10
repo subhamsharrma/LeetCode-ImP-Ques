23efde7bdd877f72ae802c4b340c8de6f3a6f716
Subham Sharrma <subhamsharrma@gmail.com>
2026-07-10 22:58:00 +0530

    Time: 0 ms (100%), Space: 13.3 MB (79.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-			cols = matrix[0].size(),
-            row = 0, col = cols - 1;
-			
-        while (row < rows && col > -1) {
-            int cur = matrix[row][col];
-            if (cur == target) return true;
-            if (target > cur) row++;
-            else col--;
+int BinarySearch(vector<int>& nums, int target){
+    int start=0,end=nums.size()-1;
+    int mid;
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]>target){
+            end=mid-1;
+        }else{
+            start=mid+1;
         }
-        
-        return false;
+    }return -1;
+}
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int start=0, end=matrix.size()-1;
+    int a=-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(matrix[mid][0]==target || matrix[mid][matrix[0].size()-1]==target){
+            return true;
+        }else if(matrix[mid][0]<target && matrix[mid][matrix[0].size()-1]>target){
+            a= BinarySearch(matrix[mid],target);
+            break;
+        }else if(matrix[mid][0]>target){
+            end=mid-1;
+        }else if(matrix[mid][matrix[0].size()-1]<target){
+            start=mid+1;
+        }
     }
+    if(a==-1){
+        return false;
+    }else{
+        return true;
+    }
+}
 };
