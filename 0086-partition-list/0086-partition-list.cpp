2a3552d1172b01886b02f1c490bbc3d49436f0a7
Subham Sharrma <subhamsharrma@gmail.com>
2026-07-18 22:35:23 +0530

    Time: 0 ms (100%), Space: 15 MB (11.2%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -12,9 +12,9 @@ class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         ListNode* slist = new ListNode(0, nullptr);
-        ListNode* blist = new ListNode(0, nullptr);
+        ListNode* list3 = new ListNode(0, nullptr);
         ListNode* small = slist;
-        ListNode* big = blist;
+        ListNode* big = list3;
 
         while (head != nullptr) {
             if (head->val < x) {
@@ -27,12 +27,12 @@ public:
             head = head->next;
         }
 
-        small->next = blist->next;
+        small->next = list3->next;
         big->next = nullptr;
 
         ListNode* result = slist->next;
         delete slist;
-        delete blist;
+        delete list3;
         return result;        
     }
 };
