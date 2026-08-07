ddee64756b45169ba5e5ce2c97e24975b0162b46
Subham Sharrma <subhamsharrma@gmail.com>
2026-08-07 23:42:49 +0530

    Time: 39 ms (76.52%), Space: 171.9 MB (78.31%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -4,17 +4,17 @@ public:
         unordered_map<string, int> mpp;
         for (string w : words) mpp[w]++;
 
-        int count = 0, alreadyPalindrome = 0;
+        int count = 0, notalready = 0;
         for (auto& [w, freq] : mpp) {
             string s = w;
             reverse(s.begin(), s.end());
             if (w == s) {
                 count += (freq / 2) * 4;
-                if (freq % 2) alreadyPalindrome = 1;
+                if (freq % 2) notalready = 1;
             } else if (w < s && mpp.count(s)) {
                 count += min(freq, mpp[s]) * 4;
             }
         }
-        return count + alreadyPalindrome * 2;
+        return count + notalready * 2;
     }
 };
