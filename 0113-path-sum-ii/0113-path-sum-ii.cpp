71d22769b3f15fb953ac3e110d5b8e3b0e546969
Subham Sharrma <subhamsharrma@gmail.com>
2026-09-12 23:28:59 +0530

    Time: 0 ms (100%), Space: 21 MB (39.75%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -7,19 +7,19 @@ public:
         return res;
     }
 
-    void dfs(TreeNode* node, int targetSum, int curSum,
+    void dfs(TreeNode* node, int targetSum, int curSuma,
              vector<int>& path, vector<vector<int>>& res) {
         if (!node) return;
 
-        curSum += node->val;
+        curSuma += node->val;
         path.push_back(node->val);
 
-        if (!node->left && !node->right && curSum == targetSum) {
+        if (!node->left && !node->right && curSuma == targetSum) {
             res.push_back(path);
         }
 
-        dfs(node->left, targetSum, curSum, path, res);
-        dfs(node->right, targetSum, curSum, path, res);
+        dfs(node->left, targetSum, curSuma, path, res);
+        dfs(node->right, targetSum, curSuma, path, res);
 
         path.pop_back();
     }
